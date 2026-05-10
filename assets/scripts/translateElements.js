37945accb999f60617997c16eceb533ebcc2bdef
anth (server) <deploy@aamediaonline.local>
2026-05-10 14:48:48 +0000

    deploy clickbox lander

---
+++ assets/scripts/translateElements.js
@@ -0,0 +1,19 @@
+import {
+    translateElements
+} from "./shared-QTME7OCH.js";
+import {
+    loadFallbackTranslation
+} from "./shared-S7PVGXSP.js";
+import "./shared-PQWVR7YF.js";
+import "./shared-IZBMCQM6.js";
+var _a, _b;
+translateElements(loadFallbackTranslation, {
+    you_have_10_seconds_tap_to_earn: {
+        macros: "{seconds}",
+        macrosValue: ((_a = APP_CONFIG.secondsLeftBeforeFinal) == null ? void 0 : _a.toString()) || "10"
+    },
+    you_have_10_seconds: {
+        macros: "{seconds}",
+        macrosValue: ((_b = APP_CONFIG.secondsLeftBeforeFinal) == null ? void 0 : _b.toString()) || "10"
+    }
+});
