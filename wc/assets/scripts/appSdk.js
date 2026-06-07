9140f0b190571183b702e8c7aaf05d086111bd07
anth <anth@local>
2026-06-07 13:42:59 +0000

    wc: clone cbs lander, add World Cup theme to title + en locale

---
+++ wc/assets/scripts/appSdk.js
@@ -0,0 +1,10 @@
+import {
+    setUrlSearchParam
+} from "./shared-ZTPKMWA6.js";
+import "./shared-CRXPGHIL.js";
+var _a;
+if (typeof window !== "undefined") {
+    window.app = Object.assign((_a = window.app) != null ? _a : {}, {
+        setUrlSearchParam
+    });
+}
